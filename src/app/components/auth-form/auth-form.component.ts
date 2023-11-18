ee935877c9e9800e06acb6f88cb408db2cc8b57f
crisjom <cristianjose.orcemontero@gmail.com>
2023-11-18 18:18:39 +0100

    Diseño preliminar de formulario de registro y página de registro

--- src/app/components/auth-form/auth-form.component.ts
+++ src/app/components/auth-form/auth-form.component.ts
@@ -1,10 +1,42 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, FormArray, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-auth-form',
   templateUrl: './auth-form.component.html',
   styleUrls: ['./auth-form.component.css']
 })
-export class AuthFormComponent {
+export class AuthFormComponent implements OnInit {
+  authForm!: FormGroup;
+  isTeacher: boolean = false;
+  availableTags: string[] = ['Matemáticas', 'Física', 'Química']; // Ejemplo de tags disponibles. CAMBIAR --> Recibir de base de datos
+  selectedTags: string[] = []; // Tags seleccionadas
 
+  constructor(private fb: FormBuilder) {}
+
+  ngOnInit() {
+    this.authForm = this.fb.group({
+      nombre: ['', [Validators.required, Validators.minLength(2)]],
+      apellidos: ['', [Validators.required, Validators.minLength(2)]],
+      fechaNacimiento: ['', Validators.required],
+      ciudad: ['', Validators.required],
+      telefono: ['', [Validators.required, Validators.pattern('^[0-9]+$')]],
+      email: ['', [Validators.required, Validators.email]],
+      password: ['', [Validators.required, Validators.minLength(6)]],
+      confirmPassword: ['', Validators.required],
+      rol: ['', Validators.required]
+    }, { validator: this.checkPasswords });
+  }
+
+  checkPasswords(group: FormGroup) {
+    const pass = group.get('password')?.value;
+    const confirmPass = group.get('confirmPassword')?.value;
+    return pass === confirmPass ? null : { notSame: true };
+  }
+
+  onSubmit() {
+    if (this.authForm.valid) {
+      console.log(this.authForm.value);
+    }
+  }
 }
