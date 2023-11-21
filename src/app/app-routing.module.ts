804dfd4cc914bf0c97505143ff48ebf67dc70b80
Bakraam <bakraam13@gmail.com>
2023-11-21 22:25:32 +0000

    Tutor-card

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { LoginPageComponent } from './pages/login-page/login-page.component';
 import { RegisterPageComponent } from './pages/register-page/register-page.component';
+import { TutorInfoPageComponent } from './pages/tutor-info-page/tutor-info-page.component';
 
 const routes: Routes = [
   {path: 'login', component: LoginPageComponent},
-  {path: 'registro', component: RegisterPageComponent}
+  { path: 'registro', component: RegisterPageComponent },
+  {path: 'tutor-info/:idTutor', component: TutorInfoPageComponent}
 ];
 
 @NgModule({
