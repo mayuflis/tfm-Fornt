efbc0e1f261420af8637fd9d2225cc8e8803c9d7
Vembrace <Vembrace@gmail.com>
2023-11-24 17:32:48 -0500

    Componente estrellas

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,9 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { LoginPageComponent } from './pages/login-page/login-page.component';
-import { TutorStarsComponent } from './components/tutor-stars/tutor-stars.component';
 
 const routes: Routes = [
   {path: 'login', component: LoginPageComponent},
-  {path: 'tutor-stars', component: TutorStarsComponent}
 ];
 
 @NgModule({
