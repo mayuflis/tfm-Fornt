8e78805b1f5b3b2f733dbe753125470909cf1371
Bakraam <bakraam13@gmail.com>
2023-11-28 21:39:06 +0000

    Aplicación del componente 'tutor-mystudent-card' en page 'tutor-my-students-page'

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -7,6 +7,7 @@ import { StudentInfoPageComponent } from './pages/student-info-page/student-info
 import { TutorProfilePageComponent } from './pages/tutor-profile-page/tutor-profile-page.component';
 import { HomePageComponent } from './pages/home-page/home-page.component';
 import { TutorMyStudentsPageComponent } from './pages/tutor-my-students-page/tutor-my-students-page.component';
+import { TutorMySubjectsPageComponent } from './pages/tutor-my-subjects-page/tutor-my-subjects-page.component';
 
 const routes: Routes = [
   {path: 'login', component: LoginPageComponent},
@@ -15,7 +16,8 @@ const routes: Routes = [
   { path: 'tutor-info/:idTutor', component: TutorInfoPageComponent },
   { path: 'student-info/:idStudent', component: StudentInfoPageComponent },
   { path: 'tutor-profile/:idTutor', component: TutorProfilePageComponent },
-  {path: 'tutor/:idTutor/students', component: TutorMyStudentsPageComponent}
+  { path: 'tutor/:idTutor/students', component: TutorMyStudentsPageComponent },
+  {path: 'tutor/:idTutor/subjects', component: TutorMySubjectsPageComponent}
 ];
 
 @NgModule({
---
+++ src/app/components/tutor-my-subjects-card/tutor-my-subjects-card.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-tutor-my-subjects-card',
+  templateUrl: './tutor-my-subjects-card.component.html',
+  styleUrls: ['./tutor-my-subjects-card.component.css']
+})
+export class TutorMySubjectsCardComponent {
+
+}
