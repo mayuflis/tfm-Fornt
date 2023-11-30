0416ea5872765b29eecbb7470dfb84bdaa45f392
SergioDev <mayitavista@gmail.com>
2023-11-30 16:09:13 +0100

    Implementación de la lógica de filtrado

--- src/app/pages/home-page/home-page.component.ts
+++ src/app/pages/home-page/home-page.component.ts
@@ -1,10 +1,20 @@
 import { Component } from '@angular/core';
+import { TeachersWebPublic } from 'src/app/interfaces/WebPublic';
 
 @Component({
   selector: 'app-home-page',
   templateUrl: './home-page.component.html',
-  styleUrls: ['./home-page.component.css']
+  styleUrls: ['./home-page.component.css'],
 })
 export class HomePageComponent {
+  filterOfTeachers!: TeachersWebPublic;
 
+  //Función que obtiene los valores filtrado del componente FilterProfessosrs
+  getFilterTeachers($event: TeachersWebPublic) {
+    this.filterOfTeachers = $event;
+    if (!$event.name) {
+      console.log('No se han encontrado profesores con los filtros aplicados');
+    }
+    console.log(this.filterOfTeachers);
+  }
 }
