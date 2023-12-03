8ffcb6ab8531cb04b93c6a01653a292a24290253
SergioDev <mayitavista@gmail.com>
2023-12-03 23:41:03 +0100

    Autenticación de usuario

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -18,37 +18,64 @@ import { StudentClassesPageComponent } from './pages/student-classes-page/studen
 import { StudentTutorsPageComponent } from './pages/student-tutors-page/student-tutors-page.component';
 import { StudentNotificationsPageComponent } from './pages/student-notifications-page/student-notifications-page.component';
 import { DetailPageComponent } from './pages/detail-page/detail-page.component';
+import { authRoleStudentsGuard } from './guards/auth-roleStudents.guard';
+import { authRoleTeachersGuard } from './guards/auth-roleTeachers.guard';
 
 const routes: Routes = [
   { path: '', pathMatch: 'full', redirectTo: 'home' },
   { path: 'home', component: HomePageComponent },
   { path: 'login', component: LoginPageComponent },
   { path: 'registro', component: RegisterPageComponent },
-  {path: 'home', component: HomePageComponent},
+  { path: 'home', component: HomePageComponent },
   { path: 'tutor-info/:idTutor', component: TutorInfoPageComponent },
   { path: 'student-info/:idStudent', component: StudentInfoPageComponent },
   { path: 'tutor-profile/:idTutor', component: TutorProfilePageComponent },
   { path: 'tutor/:idTutor/students', component: TutorMyStudentsPageComponent },
   { path: 'tutor/:idTutor/subjects', component: TutorMySubjectsPageComponent },
-  { path: 'student-profile/:idStudent', component: StudentProfilePageComponent },
-  { path: 'student/:idStudent/teachers', component: StudentMyTeachersPageComponent},
+  {
+    path: 'student-profile/:idStudent',
+    component: StudentProfilePageComponent,
+  },
+  {
+    path: 'student/:idStudent/teachers',
+    component: StudentMyTeachersPageComponent,
+  },
   { path: 'details/:idTutor', component: DetailPageComponent },
   { path: 'tutor-info/:idTutor', component: TutorInfoPageComponent },
-  { path: 'student-info/:idStudent', component: StudentInfoPageComponent },
+  {
+    path: 'student-info/:idStudent',
+    canActivate: [authRoleStudentsGuard],
+    component: StudentInfoPageComponent,
+  },
   { path: 'tutor-profile/:idTutor', component: TutorProfilePageComponent },
-  { path: 'student-profile/:idStudent', component: StudentProfilePageComponent},
+  {
+    path: 'student-profile/:idStudent',
+    component: StudentProfilePageComponent,
+  },
   { path: 'tutor-students/:idTutor', component: TutorStudentsPageComponent },
-  { path: 'tutor-classes/:idTutor', component: TutorClassesPageComponent },
-  { path: 'tutor-notifications/:idTutor', component: TutorNotificationsPageComponent},
-  { path: 'student-classes/:idStudent', component: StudentClassesPageComponent},
+  {
+    path: 'tutor-classes/:idTutor',
+    canActivate: [authRoleTeachersGuard],
+    component: TutorClassesPageComponent,
+  },
+  {
+    path: 'tutor-notifications/:idTutor',
+    component: TutorNotificationsPageComponent,
+  },
+  {
+    path: 'student-classes/:idStudent',
+    component: StudentClassesPageComponent,
+  },
   { path: 'tutor-opinions/:idTutor', component: TutorOpinionsPageComponent },
   { path: 'student-tutors/:idStudent', component: StudentTutorsPageComponent },
-  { path: 'student-notifications/:idStudent', component: StudentNotificationsPageComponent },
+  {
+    path: 'student-notifications/:idStudent',
+    component: StudentNotificationsPageComponent,
+  },
 ];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule],
 })
-
-export class AppRoutingModule { }
+export class AppRoutingModule {}
--- src/app/components/tutor-menu/tutor-menu.component.ts
+++ src/app/components/tutor-menu/tutor-menu.component.ts
@@ -1,17 +1,16 @@
-
 import { Component } from '@angular/core';
 import { Router, ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-tutor-menu',
   templateUrl: './tutor-menu.component.html',
-  styleUrls: ['./tutor-menu.component.css']
+  styleUrls: ['./tutor-menu.component.css'],
 })
 export class TutorMenuComponent {
   constructor(private router: Router, private route: ActivatedRoute) {}
 
   goToTutorInfo() {
-    this.route.paramMap.subscribe(params => {
+    this.route.paramMap.subscribe((params) => {
       const idTutor = params.get('idTutor');
       if (idTutor) {
         this.router.navigate(['/tutor-info', idTutor]);
@@ -19,7 +18,7 @@ export class TutorMenuComponent {
     });
   }
   goToTutorProfile() {
-    this.route.paramMap.subscribe(params => {
+    this.route.paramMap.subscribe((params) => {
       const idTutor = params.get('idTutor');
       if (idTutor) {
         this.router.navigate(['/tutor-profile', idTutor]);
@@ -27,7 +26,7 @@ export class TutorMenuComponent {
     });
   }
   goToTutorClasses() {
-    this.route.paramMap.subscribe(params => {
+    this.route.paramMap.subscribe((params) => {
       const idTutor = params.get('idTutor');
       if (idTutor) {
         this.router.navigate(['/tutor-classes', idTutor]);
@@ -35,7 +34,7 @@ export class TutorMenuComponent {
     });
   }
   goToTutorStudents() {
-    this.route.paramMap.subscribe(params => {
+    this.route.paramMap.subscribe((params) => {
       const idTutor = params.get('idTutor');
       if (idTutor) {
         this.router.navigate(['/tutor-students', idTutor]);
@@ -43,19 +42,19 @@ export class TutorMenuComponent {
     });
   }
   goToTutorNotifications() {
-    this.route.paramMap.subscribe(params => {
+    this.route.paramMap.subscribe((params) => {
       const idTutor = params.get('idTutor');
       if (idTutor) {
         this.router.navigate(['/tutor-notifications', idTutor]);
       }
     });
   }
-    goToTutorOpinions() {
-      this.route.paramMap.subscribe(params => {
-        const idTutor = params.get('idTutor');
-        if (idTutor) {
-          this.router.navigate(['/tutor-opinions', idTutor]);
-        }
-      });
+  goToTutorOpinions() {
+    this.route.paramMap.subscribe((params) => {
+      const idTutor = params.get('idTutor');
+      if (idTutor) {
+        this.router.navigate(['/tutor-opinions', idTutor]);
+      }
+    });
   }
 }
