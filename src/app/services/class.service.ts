bc3fe832e133125e3d8ca9360de9df00ca787cdf
SergioDev <mayitavista@gmail.com>
2023-12-11 19:16:39 +0100

    Se ha intentado añadir la funcionalidad class

---
+++ src/app/services/class.service.ts
@@ -0,0 +1,36 @@
+import { Injectable, inject } from '@angular/core';
+import { lastValueFrom } from 'rxjs';
+import { HttpClient } from '@angular/common/http';
+export type idSubject = { idsubjects: number };
+export type dataClass = {
+  fecha: string;
+  hora: string;
+  horaSumada: string;
+  idSubject: number;
+  idTutor: string;
+  idUser: number;
+  nivel: string;
+  selectedAsignatura: string;
+};
+
+@Injectable({
+  providedIn: 'root',
+})
+export class ClassService {
+  private httpClient = inject(HttpClient);
+  private baseUrl: string = 'http://localhost:3000/api/class/';
+
+  constructor() {}
+  selectedIdSubject(selectedAsignatura: string): Promise<idSubject[]> {
+    return lastValueFrom(
+      this.httpClient.get<idSubject[]>(
+        `${this.baseUrl}selectedAsignatura/${selectedAsignatura}`
+      )
+    );
+  }
+  insertClass(formValue: dataClass): Promise<number> {
+    return lastValueFrom(
+      this.httpClient.post<number>(`${this.baseUrl}insertClass`, formValue)
+    );
+  }
+}
